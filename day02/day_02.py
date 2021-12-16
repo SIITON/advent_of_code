86149ff4ae4c48f67a53d9a93daeb7cff16a00ce
Simon Tönnes <simon.tonnes@forefront.se>
2021-12-16 15:35:28 +0100

    added day2

---
+++ day02/day_02.py
@@ -0,0 +1,21 @@
+import sys
+
+data = open('input.txt').read().split('\n')
+#data = open('testinput.txt').read().split('\n')
+
+pos = 0
+depth = 0
+aim = 0
+for command in data:
+    value = int(command[-1])
+    if command[0] == 'f':
+        pos += value          # forward X increases the horizontal position by X units.
+        depth += aim * value  # increases your depth by your aim multiplied by X.
+    if command[0] == 'd':
+        #  depth += value  # Part1: down X increases the depth by X units.
+        aim += value    # down X increases your aim by X units.
+    if command[0] == 'u':
+        #  depth -= value  # Part1: up X decreases the depth by X units.
+        aim -= value    # up X decreases your aim by X units.
+
+print(pos*depth)
